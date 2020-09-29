c4b21690d1404210cc0cfd7eeac073c1bfd306fa
pastor <lucas.echeverria15@gmail.com>
2020-09-29 00:29:36 -0300

    feat: agregado de funcionalidad de eliminiacion de tarea y de lista

--- static/app.js
+++ static/app.js
@@ -2,12 +2,22 @@ const taskList = document.querySelector('#taskList');
 const taskInput = document.querySelector('#taskInput');
 const form = document.querySelector('#taskForm');
 const btnInput = document.querySelector('#btnInput')
+const clearBtn = document.querySelector('#clearBtn')
 
 // agregar eventlisteners
 form.addEventListener('submit', addTask);
 btnInput.addEventListener('click', addTask);
+taskList.addEventListener('click', removeTask);
+clearBtn.addEventListener('click', clearTasks);
+
 
 function addTask(e){
+
+    // chequear por submit vacio
+    if (taskInput.value === ''){
+        alert('No podes agregar una tarea vacia');
+        return false; // por algun motivo el alert aparece dos veces # Consultar
+    }
 
     // crear un li
     const li = document.createElement('li');
@@ -41,4 +51,26 @@ function addTask(e){
 }
 
 
+function removeTask(e){
 
+    // El target es el <i>, pero la clase la tengo en el link, que es el padre
+    // por eso pregunto si el padre del target contiene esa clase
+    if(e.target.parentElement.classList.contains('delete-item')){
+
+        // remuevo al "abuelo" del <i> que seria el <li>
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+
+function clearTasks(){
+
+    // consultar por la confirmacion de eliminacion de tareas
+    if(confirm('Estas seguro?')){
+
+        // mientras la lista de tareas tenga un elemento hijo, lo elimino
+        while(taskList.firstChild){
+            taskList.removeChild(taskList.firstChild);
+        }
+    }
+}
